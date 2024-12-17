794d98d664a6e087f3b8ce0a9daec5a7caedfcec
HimanshuInRaoInfo <himanshu@raoinformationtechnology.com>
2024-12-17 16:55:39 +0530

    Add return browser list saving function

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class GetSuperseeWindow {
     constructor() { }
 
     setBrowsersList(browserLists) {
-        this.fileOperation.saveBrowserList(browserLists);
+        return this.fileOperation.saveBrowserList(browserLists);
     }
 
     getBrowserList() {
